c675b755a7aa3d2ae6fdec3678692f31a8fb610e
Nick <nick@Sams-MacBook-Pro.local>
2017-11-13 17:50:34 +0100

    :construction: Implemented authentication halfway.

--- episode11/app/Comment.php
+++ episode11/app/Comment.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    public function getPost()
+    protected $guarded = [];
+
+    public function post()
     {
         return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- episode11/resources/views/posts/show.blade.php
+++ episode11/resources/views/posts/show.blade.php
@@ -27,7 +27,22 @@
             </ul>
         </div>
 
-        <card></card>
+        <hr>
+
+        <div class="card">
+            <div class="card-block">
+                <form action="/posts/{{ $post->id }}/comments" method="POST">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" placeholder="Your comment here" class="form-control" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add comment</button>
+                    </div>
+                </form>
+                @include('layouts.errors')
+            </div>
+        </div>
     </div>
 @endsection
 
